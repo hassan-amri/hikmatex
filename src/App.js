54008018be56aaf0364a92232528a627e2b2085a
hassan <hassan123@gmail.com>
2024-10-11 21:50:41 +0100

    header completed successfully

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Header";
+import { BrowserRouter, Route ,Routes} from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+
+        <div className="pages">
+          <Routes>
+            
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About/>} />
+            {/* <Route path="/blog" component={Blog} /> */}
+            {/* <Route path="/contact" component={Contact} /> */}
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Header from "./Header";
+
+function Home() {
+  return <div>
+    <h1>home</h1>
+  </div>
+}
+
+export default Home;
